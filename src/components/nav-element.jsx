7c2349bcb4a3e8c11818399eb13f077e6f015b17
Ethan Davis <wetheethan@gmail.com>
2020-03-15 18:17:08 -0500

    Created nav element component - fixed bootstrap actions not working

---
+++ src/components/nav-element.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class NavElement extends Component {
+	state = {  };
+	render() { 
+		return ( <NavElement></NavElement> );
+	}
+}
+ 
+export default NavElement;
