f5fd5d8ce6fa09e580e6014268eb8a472a84ce71
Ethan Davis <wetheethan@gmail.com>
2020-03-21 16:31:06 -0500

    Fixing typo in file naem

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,7 +10,6 @@ class Counter extends Component {
 	render() {
 		return (
 			<div>
-				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm" onClick={() => { this.props.onIncrement(this.props.counter) }}>Increment</button>
 				<button className="btn btn-danger sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
 			</div>
