75530b2e8c278076384efcc42ecb3128f7cf692f
Ethan Davis <wetheethan@gmail.com>
2020-04-04 16:58:40 -0500

    Added item details to page

--- src/components/inventory-item-details/inventory-item-details.jsx
+++ src/components/inventory-item-details/inventory-item-details.jsx
@@ -20,12 +20,20 @@ const InventoryItemDetails = ({ match }) => {
 	});
 
 	return (
-		<div>
-			<Button type="button" key={`btn-${item.id}`}
-				buttonStyle="btn--primary--solid"
-				buttonSize="btn--medium"
-				onClick={() => { dispatch({ type: "ADD_TO_CHART", payload: item }) }}>Add To Chart</Button>
+		<div className="row">
+			<div className="col-md-6">
+				<img className="img-responsive" src={item.image} alt="image cap" />
+			</div>
+			<div className="col-md-6">
+				<h2>{item.name}</h2>
+				<p>{item.price}</p>
+				<Button type="button" key={`btn-${item.id}`}
+					buttonStyle="btn--primary--solid"
+					buttonSize="btn--medium"
+					onClick={() => { dispatch({ type: "ADD_TO_CHART", payload: item }) }}>Add To Chart</Button>
+			</div>
 		</div>
+
 	);
 }
 
