576322cb5f0e54934fc8c06d341e8698f38080f5
Ethan Davis <wetheethan@gmail.com>
2020-04-09 18:55:28 -0500

    Cleaned up product details page

--- src/components/inventory-item-details/inventory-item-details.jsx
+++ src/components/inventory-item-details/inventory-item-details.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable react/prop-types */
-import React, { createContext, useEffect, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer, useState } from 'react';
 import httpService from "../../services/http-service";
 import Button from '../button/button';
 import { inventoryItemDetailsReducer } from './inventory-item-details.reducer';
-
+import "./inventory-item-details.less"
+import { itemsChartReducer } from '../items-chart/items-chart.reducer';
 export const ItemsDetailsContext = createContext();
+
+const formatCurrency = (amount) => {
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	});
+
+	return formatter.format(amount)
+}
 
 const InventoryItemDetails = ({ match }) => {
 
@@ -19,17 +29,23 @@ const InventoryItemDetails = ({ match }) => {
 	}, []);
 
 	return (
-		<div className="row">
-			<div className="col-md-6">
-				<img className="img-responsive" src={item.image} alt="image cap" />
-			</div>
-			<div className="col-md-6">
-				<h2>{item.name}</h2>
-				<p>{item.price}</p>
-				<Button type="button" key={`btn-${item.id}`}
-					buttonStyle="btn--primary--solid"
-					buttonSize="btn--medium"
-					onClick={() => { dispatch({ type: "ADD_TO_CHART", payload: item }) }}>Add To Chart</Button>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-6">
+					<img className="img-responsive" src={item.image} alt="image cap" />
+				</div>
+				<div className="col-md-6 justify-content-center text-center item-details">
+					<div className="item-details-name">
+						<h2>{item.name}</h2>
+					</div>
+					<div className="item-details-price">
+						<p>{formatCurrency(item.price)}</p>
+					</div>
+					<Button type="button" key={`btn-${item.id}`}
+						buttonStyle="btn--primary--solid"
+						buttonSize="btn--large"
+						onClick={() => { dispatch({ type: "ADD_TO_CHART", payload: item }) }}>Add To Chart</Button>
+				</div>
 			</div>
 		</div>
 
