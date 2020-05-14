4bb1adec7399b7ab7cadbf0609a8d9052730e509
Ethan Davis <wetheethan@gmail.com>
2020-05-13 21:00:02 -0500

    Removed dead code from item chart component

--- src/components/items-chart/items-chart.jsx
+++ src/components/items-chart/items-chart.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import React, { useContext } from 'react';
-import Button from '../button/button';
 import InventoryItemCard from '../inventory-item-card/inventory-item-card';
 import { ItemsChartContext } from '../../context/items-chart-context';
 
@@ -10,14 +9,7 @@ const ItemsChart = () => {
 		<div className="row justify-content-center align-items-center">
 			{
 				chartState.map(item =>
-					<InventoryItemCard key={item.id}
-						item={item}>
-						<Button onClick={() => deleteFromChart(item.id)}
-							type="button" key={`btn-${item.id}`}
-							buttonStyle="btn--danger--solid"
-							buttonSize="btn--medium">
-								</Button>
-					</InventoryItemCard>
+					<InventoryItemCard key={item.id} item={item}/ >
 				)}
 		</div>
 	);
