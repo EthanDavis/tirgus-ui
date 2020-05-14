1f429e38300f358f79b266ced2b7c41cedf9fe38
Ethan Davis <wetheethan@gmail.com>
2020-05-13 20:54:49 -0500

    Removed lint disable

--- src/components/navigation/navigation-bar.jsx
+++ src/components/navigation/navigation-bar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useContext } from 'react';
