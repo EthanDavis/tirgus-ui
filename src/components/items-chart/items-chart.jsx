ba235ad8292a88fde9c2bb03e4f5d8db54a92594
Ethan Davis <wetheethan@gmail.com>
2020-05-19 12:15:45 -0500

    Added remove from chart functionality - Moved duplicated code into helper function in item chart reducer

--- src/components/items-chart/items-chart.jsx
+++ src/components/items-chart/items-chart.jsx
@@ -1,14 +1,21 @@
 import React, { useContext } from 'react';
 import InventoryItemCard from '../inventory-item-card/inventory-item-card';
 import { ItemsChartContext } from '../../context/items-chart-context';
+import Button from '../button/button';
 
 const ItemsChart = () => {
-	const { chartState } = useContext(ItemsChartContext);
+	const { chartState, itemsChartDispatch } = useContext(ItemsChartContext);
 	return (
 		<div className="row justify-content-center align-items-center">
 			{
 				chartState.map(chartItem =>
-					<InventoryItemCard key={chartItem.item.id} item={chartItem.item}/ >
+					<React.Fragment key={chartItem.item.id} >
+						<InventoryItemCard item={chartItem.item} />
+						<Button type="button" key={`btn-${chartItem.item.id}`}
+							buttonStyle="btn--primary--solid"
+							buttonSize="btn--large"
+							onClick={() => { itemsChartDispatch({ type: "DELETE_FROM_CHART", payload: chartItem }) }}>Remove From Chart</Button>
+					</React.Fragment>
 				)}
 		</div>
 	);
--- src/components/items-chart/items-chart.reducer.js
+++ src/components/items-chart/items-chart.reducer.js
@@ -1,13 +1,8 @@
 export const itemsChartReducer = (chartState, action) => {
 	switch (action.type) {
 		case "ADD_TO_CHART":
-			let itemIdx = -1;
-			for (let i = 0; i < chartState.length; i++) {
-				if (chartState[i].item.id === action.payload.item.id) {
-					itemIdx = i;
-				}
-			}
 			const chartStateCopy = [].concat(chartState);
+			const itemIdx = getItemIndex(chartStateCopy, action.payload)
 			if (itemIdx != -1) {
 				chartStateCopy[itemIdx].count++;
 			} else {
@@ -16,9 +11,27 @@ export const itemsChartReducer = (chartState, action) => {
 			chartState = chartStateCopy
 			return chartState
 		case "DELETE_FROM_CHART":
-			const idx = chartState.indexOf(action.payload.id);
-			chartState.splice(0, idx)
+			const newChartState = [].concat(chartState);
+			const idx = getItemIndex(newChartState, action.payload)
+
+			if (newChartState[idx].count === 1) {
+				newChartState.splice(idx, 1)
+			} else {
+				newChartState[idx].count--;
+			}
+			chartState = newChartState;
+			return chartState;
 		default:
 			return chartState;
 	}
 };
+
+const getItemIndex = (chartState, payload) => {
+	let itemIdx = -1;
+	for (let i = 0; i < chartState.length; i++) {
+		if (chartState[i].item.id === payload.item.id) {
+			itemIdx = i;
+		}
+	}
+	return itemIdx;
+}
