1b688464a875a411fff13ed1891be02e37cbc442
Ethan Davis <wetheethan@gmail.com>
2020-05-18 23:30:33 -0500

    Removed console logs

--- src/components/items-chart/items-chart.reducer.js
+++ src/components/items-chart/items-chart.reducer.js
@@ -3,14 +3,11 @@ export const itemsChartReducer = (chartState, action) => {
 		case "ADD_TO_CHART":
 			let itemIdx = -1;
 			for (let i = 0; i < chartState.length; i++) {
-				console.log("Payload", action.payload.item);
-				console.log("chart", chartState[i]);
 				if (chartState[i].item.id === action.payload.item.id) {
 					itemIdx = i;
 				}
 			}
 			const chartStateCopy = [].concat(chartState);
-			console.log("test", itemIdx)
 			if (itemIdx != -1) {
 				chartStateCopy[itemIdx].count++;
 			} else {
